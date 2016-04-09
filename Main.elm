83c576cf90fbf849a2b2613581f1844fd13665d4
Mike Situ <msitu@splunk.com>
2016-04-09 12:09:05 -0700

    stubbed data and displaying basic view. no logic yet

--- Main.elm
+++ Main.elm
@@ -1,18 +1,85 @@
 module Main (..) where
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 type alias Model =
-  Int
+  { responses : List ServerResponse
+  }
 
 
+type alias ServerResponse =
+  { id : Int
+  , user : String
+  , name : String
+  , namespace :
+      String
+      -- , status : Int
+  , description : String
+  , is_private :
+      Bool
+      -- , is_automated : Bool
+      -- , can_edit : Bool
+      -- , star_count : Int
+      -- , pull_count : Int
+      -- , last_updated : String
+  }
+
+
+model : Model
 model =
-  3
+  { responses =
+      [ { id = 1
+        , user = "mikesi2"
+        , name = "primeroProyecto"
+        , namespace = "nombreEspacio"
+        , description = "some really cool stuff is gonna happen"
+        , is_private = True
+        }
+      , { id = 2
+        , user = "mikesi2"
+        , name = "segundoProyecto"
+        , namespace = "nombreEspacio"
+        , description = "if you didn't believe... you will!"
+        , is_private = False
+        }
+      , { id = 3
+        , user = "mikesi2"
+        , name = "another project"
+        , namespace = "kevinspacey"
+        , description = "if you didn't believe... you will!"
+        , is_private = False
+        }
+      ]
+  }
 
 
+view : Model -> Html
 view model =
-  div [] [ text "hi mike" ]
+  div
+    [ class "content" ]
+    [ header [] [ h1 [] [ text "like Docker Hub, but in Elm" ] ]
+    , ul
+        []
+        (List.map viewResponse model.responses)
+    ]
+
+
+viewResponse : ServerResponse -> Html
+viewResponse response =
+  li
+    []
+    [ a
+        [ href "something" ]
+        [ span [] [ text (response.namespace ++ " / " ++ response.name) ]
+        , (if response.is_private then
+            span [] [ text "private" ]
+           else
+            span [] []
+          )
+        ]
+    ]
 
 
 main =
