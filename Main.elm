b06c2b84b60ef7aaee6951de3b87153c86a61403
Mike Situ <msitu@splunk.com>
2016-04-09 21:52:23 -0700

    filtering, limit to 10 responses, next/prev paginator w/o logic

--- Main.elm
+++ Main.elm
@@ -2,10 +2,19 @@ module Main (..) where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Signal exposing (Address)
+import StartApp.Simple as StartApp
+import Json.Decode as Json
+import Task exposing (Task)
+import Effects exposing (Effects)
+import String
 
 
 type alias Model =
-  { responses : List ServerResponse
+  { filterStr : String
+  , pageNum : Int
+  , responses : List ServerResponse
   }
 
 
@@ -27,9 +36,14 @@ type alias ServerResponse =
   }
 
 
-model : Model
-model =
-  { responses =
+
+-- initialModel : Model
+
+
+initialModel =
+  { filterStr = ""
+  , pageNum = 1
+  , responses =
       [ { id = 1
         , user = "mikesi2"
         , name = "primeroProyecto"
@@ -46,8 +60,71 @@ model =
         }
       , { id = 3
         , user = "mikesi2"
-        , name = "another project"
+        , name = "another-project"
         , namespace = "kevinspacey"
+        , description = "if you didn't believe... you will!"
+        , is_private = False
+        }
+      , { id = 4
+        , user = "outcoldman"
+        , name = "splunk"
+        , namespace = "outcoldman"
+        , description = "if you didn't believe... you will!"
+        , is_private = False
+        }
+      , { id = 5
+        , user = "mikesi2"
+        , name = "primeroProyecto"
+        , namespace = "nombreEspacio"
+        , description = "some really cool stuff is gonna happen"
+        , is_private = True
+        }
+      , { id = 6
+        , user = "mikesi2"
+        , name = "segundoProyecto"
+        , namespace = "nombreEspacio"
+        , description = "if you didn't believe... you will!"
+        , is_private = False
+        }
+      , { id = 7
+        , user = "mikesi2"
+        , name = "another-project"
+        , namespace = "kevinspacey"
+        , description = "if you didn't believe... you will!"
+        , is_private = False
+        }
+      , { id = 8
+        , user = "outcoldman"
+        , name = "splunk"
+        , namespace = "outcoldman"
+        , description = "if you didn't believe... you will!"
+        , is_private = False
+        }
+      , { id = 9
+        , user = "mikesi2"
+        , name = "primeroProyecto"
+        , namespace = "nombreEspacio"
+        , description = "some really cool stuff is gonna happen"
+        , is_private = True
+        }
+      , { id = 10
+        , user = "mikesi2"
+        , name = "segundoProyecto"
+        , namespace = "nombreEspacio"
+        , description = "if you didn't believe... you will!"
+        , is_private = False
+        }
+      , { id = 11
+        , user = "mikesi2"
+        , name = "another-project"
+        , namespace = "kevinspacey"
+        , description = "if you didn't believe... you will!"
+        , is_private = False
+        }
+      , { id = 12
+        , user = "outcoldman"
+        , name = "splunk"
+        , namespace = "outcoldman"
         , description = "if you didn't believe... you will!"
         , is_private = False
         }
@@ -55,46 +132,92 @@ model =
   }
 
 
-view : Model -> Html
-view model =
+type Action
+  = FilterByName String
+
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    FilterByName str ->
+      { model | filterStr = str }
+
+
+viewFilter : Address Action -> String -> Html
+viewFilter address str =
+  input
+    [ class "filter-repo"
+    , value str
+    , onInput address FilterByName
+    , placeholder "All Accounts"
+    ]
+    []
+
+
+view : Address Action -> Model -> Html
+view address model =
   div
     [ class "content" ]
-    [ header
-        []
-        [ h1 [] [ text "like Docker Hub, but in Elm" ]
-        , (viewFilter model.responses)
-        ]
-    , ul
-        []
-        (List.map viewResponse model.responses)
+    [ (viewHeader)
+    , (viewFilter address model.filterStr)
+    , (viewResponses address model)
+    , (viewPaginator address model)
     ]
 
 
-viewFilter responses =
-  select
+onInput address wrap =
+  on "input" targetValue (\val -> Signal.message address (wrap val))
+
+
+viewHeader : Html
+viewHeader =
+  header
     []
-    (List.map viewOption responses)
+    [ h1 [] [ text "like Docker Hub, but in Elm" ] ]
 
 
-viewOption response =
-  option [] [ text response.namespace ]
+viewPaginator : Address Action -> Model -> Html
+viewPaginator address model =
+  div
+    []
+    [ button [] [ text "<" ]
+    , span [ class "current-page" ] [ text (toString model.pageNum) ]
+    , button [] [ text ">" ]
+    ]
+
+
+viewResponses : Address Action -> Model -> Html
+viewResponses address { filterStr, pageNum, responses } =
+  ul
+    []
+    (responses
+      |> List.filter
+          (\r ->
+            (String.startsWith filterStr r.namespace)
+              || (String.isEmpty filterStr == True)
+          )
+      |> List.drop ((pageNum - 1) * 10)
+      |> List.take 10
+      |> List.map viewResponse
+    )
 
 
 viewResponse : ServerResponse -> Html
 viewResponse response =
   li
     []
-    [ a
-        [ href "something" ]
-        [ span [] [ text (response.namespace ++ " / " ++ response.name) ]
-        , (if response.is_private then
-            span [] [ text "  private" ]
-           else
-            span [] []
-          )
-        ]
+    [ span [] [ text (response.namespace ++ " / " ++ response.name) ]
+    , (if response.is_private then
+        span [] [ text "  private" ]
+       else
+        span [] []
+      )
     ]
 
 
 main =
-  view model
+  StartApp.start
+    { view = view
+    , update = update
+    , model = initialModel
+    }
